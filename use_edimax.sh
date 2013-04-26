fe97baa5ebe37ef03c583e5c43c32c4a7ec55500
Jamie Alexandre <jamalex@gmail.com>
2013-04-26 14:05:39 -0700

    Scripts to toggle between the hostapd binary/conf for Edimax/Wi-Pi.

---
+++ use_edimax.sh
@@ -0,0 +1,25 @@
+# stop hostapd before replacing it
+sudo service hostapd stop
+
+# backup the hostapd binary if needed
+if [ ! -f /usr/sbin/hostapd_original ];
+then
+    sudo mv /usr/sbin/hostapd /usr/sbin/hostapd_original
+fi
+
+# use the binary hostapd for the Edimax EW-7811Un
+sudo rm /usr/sbin/hostapd
+sudo ln -s conf/hostapd_RTL8188CUS /usr/sbin/hostapd
+
+# backup the hostapd config file if needed
+if [ ! -f /etc/hostapd/hostapd_original.conf ];
+then
+    sudo mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd_original.conf
+fi
+
+# use the hostapd config file for the Edimax EW-7811Un
+sudo rm /etc/hostapd/hostapd.conf
+sudo ln -s conf/hostapd_RTL8188CUS.conf /etc/hostapd/hostapd.conf
+
+# restart hostapd with the new settings
+sudo service hostapd start
---
+++ use_wipi.sh
@@ -0,0 +1,21 @@
+# stop hostapd before replacing it
+sudo service hostapd stop
+
+# if we're not already using the original, switch
+if [ -f /usr/sbin/hostapd_original ];
+then
+    # use the original binary hostapd, for the Wi-Pi
+    sudo rm /usr/sbin/hostapd
+    sudo ln -s /usr/sbin/hostapd_original /usr/sbin/hostapd
+fi
+
+# if we're not already using the original conf, switch
+if [ -f /etc/hostapd/hostapd_original.conf ];
+then
+    # use the hostapd config file for the Edimax EW-7811Un
+    sudo rm /etc/hostapd/hostapd.conf
+    sudo ln -s /etc/hostapd/hostapd_original.conf /etc/hostapd/hostapd.conf
+fi
+
+# restart hostapd with the new settings
+sudo service hostapd start
